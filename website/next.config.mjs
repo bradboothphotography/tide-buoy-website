7d7d4a4e1825cb01bac9a24b2060b034c50350c6
William Booth <bradboothphotography@gmail.com>
2026-06-23 09:57:24 -0800

    force canonical website host

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,4 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.tidebuoy.com"
+          }
+        ],
+        destination: "https://tidebuoy.com/:path*",
+        permanent: true
+      }
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "index, follow"
+          }
+        ]
+      }
+    ];
+  }
+};
 
 export default nextConfig;
